311eaffd2a19044bf12ccbf563bd61b8c383ae8a
Roaringcows <henry.codewei@gmail.com>
2022-08-31 19:10:10 -0700

    Drawing the board, Moving the Pieces, Undo moves and working on generating Valid Chess Moves

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,81 @@
+#This class is reponsible for the storing all the info about the current state of a chess game. It will also be responsible for determining the valid moves at the current state.It will also keep a move log.
+
+
+
+class GameState():
+    def __init__(self):
+        #board is an 8x8 2d list, each element of the list has 2 charachters.
+        #First character represents the color of the piece
+        #Second character represents the type of the piece, "R","N","B","Q","K" or "p"
+        #"--" - represents an empty space with no piece
+        self.board=[
+            ["bR","bN","bB","bQ","bK","bB","bN","bR"],
+            ["bp","bp","bp","bp","bp","bp","bp","bp"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["wp","wp","wp","wp","wp","wp","wp","wp"],
+            ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
+        self.whiteToMove = True
+        self.moveLog = []
+
+
+    '''
+    Takes a Move as a parameter and executes it (will not work for castling, pawn promotion, and en-passant)
+    '''
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move) #log the move to we can undo or display history
+        self.whiteToMove = not self.whiteToMove #swap players turns
+
+
+    '''
+    Undo the last move made
+    '''
+    def undoMove(self):
+        if len(self.moveLog) != 0: #make sure theres a move to undo
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove #switched turns back
+    
+
+    '''
+    ALL moves considering checks
+    '''
+    def getValidMoves(self):
+        return self.getAllPossibleMoves() #for now we will not worry about checks
+
+    '''
+    ALL moves without considering checks
+    '''
+    def getAllPossibleMoves(self):
+        pass
+        
+
+class Move():
+    # maps keys to values
+    # key : value
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4,
+                   "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3,
+                   "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        #add to this code to make real chess notation
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
